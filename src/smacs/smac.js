e5b9b86115c619e24e85ddfa4e6c1ae3dbb72abe
apierro <antonio.pierro@gmail.com>
2020-02-03 10:40:42 +0100

    printing error on console

--- src/smacs/smac.js
+++ src/smacs/smac.js
@@ -1,6 +1,5 @@
 import requiredParam from "../helpers/required-param";
 import { InvalidPropertyError } from "../helpers/errors";
-import isValidEmail from "../helpers/is-valid-email.js";
 import upperFirst from "../helpers/upper-first";
 
 export default function makeSmac(contactInfo = requiredParam("contactInfo")) {
@@ -8,17 +7,9 @@ export default function makeSmac(contactInfo = requiredParam("contactInfo")) {
     const normalContact = normalize(validContact);
     return Object.freeze(normalContact);
 
-    function validate({
-        hash = requiredParam("hash"),
-        // lastName = requiredParam("lastName"),
-        // emailAddress = requiredParam("emailAddress"),
-        ...otherInfo
-    } = {}) {
+    function validate({ hash = requiredParam("hash"), ...otherInfo } = {}) {
         validateHash("hash", hash);
-        // validateName("last", lastName);
-        // validateEmail(emailAddress);
         return { hash, ...otherInfo };
-        return { firstName, lastName, emailAddress, ...otherInfo };
     }
 
     function validateHash(label, hash) {
@@ -29,18 +20,10 @@ export default function makeSmac(contactInfo = requiredParam("contactInfo")) {
         }
     }
 
-    function validateEmail(emailAddress) {
-        if (!isValidEmail(emailAddress)) {
-            throw new InvalidPropertyError("Invalid contact email address.");
-        }
-    }
-
     function normalize({ emailAddress, hash, lastName, ...otherInfo }) {
         return {
             ...otherInfo,
             hash: upperFirst(hash)
-            // lastName: upperFirst(lastName)
-            // emailAddress: emailAddress.toLowerCase()
         };
     }
 }
