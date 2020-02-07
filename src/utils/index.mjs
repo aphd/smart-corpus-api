fbb8906af510056717d105ab78d3b7c138408d2f
apierro <antonio.pierro@gmail.com>
2020-02-07 18:33:20 +0100

    loading port from env with dotenv.config

--- src/utils/index.mjs
+++ src/utils/index.mjs
@@ -1,10 +1,14 @@
 import smacPost from "./post-request.mjs";
 import readCSV from "./read-csv.mjs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 readCSV("/tmp/test.csv", data => {
+    console.log("process.env.PORT: ", process.env.PORT);
     smacPost(JSON.stringify(data), {
         hostname: "localhost",
-        port: 9090,
+        port: process.env.PORT,
         path: "/smacs",
         method: "POST",
         headers: {
