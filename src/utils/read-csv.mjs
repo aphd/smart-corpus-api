00411da50623cfb7a4e9fab10c9b2aa752d5a616
apierro <antonio.pierro@gmail.com>
2020-02-11 09:08:54 +0100

    refactoring smacPost request

--- src/utils/read-csv.mjs
+++ src/utils/read-csv.mjs
@@ -4,13 +4,8 @@ import fs from "fs";
 export default function readCSV(fn, callback) {
     const results = [];
     fs.createReadStream(fn)
-        .pipe(csv())
-        .on("data", data =>
-            results.push({
-                address: data["ContractAddress"],
-                name: data["ContractName"]
-            })
-        )
+        .pipe(csv({ separator: ";" }))
+        .on("data", data => results.push(data))
         .on("end", () => {
             callback(results);
         });
