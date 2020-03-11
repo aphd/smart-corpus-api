d9b63f98764e99c8162a8f33583c6b3e48487072
apierro <antonio.pierro@gmail.com>
2020-03-11 12:46:54 +0100

    added download module

---
+++ src/utils/download.mjs
@@ -0,0 +1,15 @@
+import https from "https";
+import fs from "fs";
+
+export default function download(url, dir, fn, cb) {
+    !fs.existsSync(dir) && fs.mkdirSync(dir);
+    const dest = dir + fn;
+    if (fs.existsSync(dest)) return 0;
+    let file = fs.createWriteStream(dest);
+    https.get(url, function(response) {
+        response.pipe(file);
+        file.on("finish", function() {
+            file.close(cb); // close() is async, call cb after close completes.
+        });
+    });
+}
