6831342c5be9887f4ab1dc1056ea5420024ffbdd
apierro <antonio.pierro@gmail.com>
2020-03-11 12:53:21 +0100

    moving apikey in .env

--- src/utils/download.mjs
+++ src/utils/download.mjs
@@ -1,11 +1,15 @@
 import https from "https";
 import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default function download(url, dir, fn, cb) {
     !fs.existsSync(dir) && fs.mkdirSync(dir);
     const dest = dir + fn;
     if (fs.existsSync(dest)) return 0;
     let file = fs.createWriteStream(dest);
+    url = `${url}&apikey=${process.env.API_KEY}`;
     https.get(url, function(response) {
         response.pipe(file);
         file.on("finish", function() {
