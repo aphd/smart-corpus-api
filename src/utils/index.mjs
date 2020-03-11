3cca15e87b8528ac58c7eb517139b6eefc8a9532
apierro <antonio.pierro@gmail.com>
2020-03-11 11:43:54 +0100

    Added mapHeaders to readCSV function

--- src/utils/index.mjs
+++ src/utils/index.mjs
@@ -4,4 +4,9 @@ import readCSV from "./read-csv.mjs";
 const w_callback = data => {
     smacPost(JSON.stringify(data));
 };
-readCSV("/tmp/smec.test.csv", w_callback); // https://github.com/aphd/solidity-metrics/blob/master/examples/smec.csv
+// readCSV("/tmp/smec.test.csv", w_callback); // https://github.com/aphd/solidity-metrics/blob/master/examples/smec.csv
+
+readCSV("./src/csv/export-verified-contractaddress-opensource-license.csv", {
+    callback: w_callback,
+    columns_to_skip: ["Txhash"]
+});
