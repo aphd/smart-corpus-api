917400444802a38cffd4cd345ebae32f80234b69
apierro <antonio.pierro@gmail.com>
2020-03-13 18:26:32 +0100

    removing post-request for axios

--- src/utils/client.mjs
+++ src/utils/client.mjs
@@ -1,27 +1,33 @@
-import postRequest from "./post-request.mjs";
 import axios from "axios";
 import readCSV from "./read-csv.mjs";
 import download from "./download.mjs";
 
-const read_csv = function() {
-    // readCSV("/tmp/smec.test.csv", w_callback); // https://github.com/aphd/solidity-metrics/blob/master/examples/smec.csv
-    readCSV(
-        "./src/csv/export-verified-contractaddress-opensource-license.csv",
-        ["Txhash"]
-    ).then(result => console.log(result));
+// TODO to take from config-file
+const server = "http://localhost:8080/";
+const source =
+    "https://api.etherscan.io/api?module=contract&action=getsourcecode&address=";
+const verified_address =
+    "./src/csv/export-verified-contractaddress-opensource-license.csv";
+
+const post_addresses = function() {
+    const [fn, columns_to_skip] = [verified_address, ["Txhash"]];
+    readCSV(fn, columns_to_skip).then(result =>
+        axios.post(server, result).catch(error => console.log(error))
+    );
 };
 
 const download_contracts = function() {
     const options = address => ({
-        url: `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${address}`,
+        url: `${source}${address}`,
         dir: `./src/json/${address.substring(0, 4)}/`,
         fn: `${address}.json`
     });
-    axios.get("http://localhost:8080/").then(function(response) {
+    axios.get(server).then(function(response) {
         response.data.forEach(e =>
             download(options(e.contractAddress.toLowerCase()))
         );
     });
 };
 
-read_csv();
+post_addresses();
+// download_contracts();
