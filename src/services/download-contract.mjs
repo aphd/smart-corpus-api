448ca5896fb15d6e62a6aa2be480d0cb6469046e
apierro <antonio.pierro@gmail.com>
2020-03-17 19:22:48 +0100

    renaming download-contract methods

--- src/services/download-contract.mjs
+++ src/services/download-contract.mjs
@@ -1,25 +1,20 @@
 import https from "https";
 import fs from "fs";
 import dotenv from "dotenv";
-import axios from "axios";
 import { delayLoop, urlDirFn } from "../utils/index.mjs";
+import { getAddresses } from "./index.mjs";
 
 dotenv.config();
 
-let server =
-    'http://localhost:8080/{"$or":[  {"contractAddress":"0x9e1ae1cae3e9609626753086d4859cddc33f1cc8"}, {"contractAddress":"0x6fa85aa3f44f296de1294e3c097872235215b822"}]}';
-
-server = "http://localhost:8080/";
-
 export default function downloadContracts() {
-    axios.get(server).then(function(response) {
-        response.data.forEach(
+    getAddresses().then(addresses =>
+        addresses.data.forEach(
             delayLoop(
                 e => download(urlDirFn(e.contractAddress.toLowerCase())),
                 200
             )
-        );
-    });
+        )
+    );
 }
 
 const download = url_dir_fn => {
