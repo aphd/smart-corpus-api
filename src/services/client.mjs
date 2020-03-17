ebabd39b64b9da031fa23b99f6fe0c7cf1bf803b
apierro <antonio.pierro@gmail.com>
2020-03-17 19:32:41 +0100

    renaming writeMetrics method

--- src/services/client.mjs
+++ src/services/client.mjs
@@ -1,20 +1,5 @@
-import axios from "axios";
 import { postAddresses, downloadContracts, writeMetrics } from "./index.mjs";
 
-// TODO to take from config-file
-
-const write_metrics = function() {
-    axios
-        .get(server)
-        .then(function(response) {
-            response.data.forEach(e => {
-                const contractAddress = e.contractAddress.toLowerCase();
-                let { dir, fn } = url_dir_fn(contractAddress);
-                writeMetrics(dir + fn, contractAddress);
-            });
-        })
-        .catch(e => console.log(e));
-};
 // postAddresses();
-downloadContracts();
-//write_metrics();
+// downloadContracts();
+writeMetrics();
