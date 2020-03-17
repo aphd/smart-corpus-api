eb4a6284692dae9cb7586e833d56521849d3fcf9
apierro <antonio.pierro@gmail.com>
2020-03-17 19:32:41 +0100

    renaming writeMetrics method

--- src/services/write-metrics.mjs
+++ src/services/write-metrics.mjs
@@ -1,6 +1,8 @@
 import paso from "./paso";
 import fs from "fs";
 import ObjectsToCsv from "objects-to-csv";
+import { getAddresses } from "./index.mjs";
+import { urlDirFn } from "../utils/index.mjs";
 
 // TODO import from a conf file
 const fn_metric = "./src/csv/paso_metric.csv";
@@ -27,11 +29,22 @@ const get_json_metrics_from_sol = dest =>
         })
     );
 
-export default function writeMetrics(dest, contractAddress) {
+const writeMetricsOfOneContract = (dest, contractAddress) =>
     fs.existsSync(dest) &&
-        get_json_metrics_from_sol(dest).then(data => {
-            data.contractAddress = contractAddress;
-            const csv = new ObjectsToCsv([data]);
-            csv.toDisk(fn_metric, { append: true }); // Save to file:
-        });
+    get_json_metrics_from_sol(dest).then(data => {
+        data.contractAddress = contractAddress;
+        const csv = new ObjectsToCsv([data]);
+        csv.toDisk(fn_metric, { append: true }); // Save to file:
+    });
+
+export default function writeMetrics() {
+    getAddresses()
+        .then(function(response) {
+            response.data.forEach(e => {
+                const contractAddress = e.contractAddress.toLowerCase();
+                let { dir, fn } = urlDirFn(contractAddress);
+                writeMetricsOfOneContract(dir + fn, contractAddress);
+            });
+        })
+        .catch(e => console.log(e));
 }
