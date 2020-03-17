c4e276c60465b380dfe8e72c51cb2e8de9708f20
apierro <antonio.pierro@gmail.com>
2020-03-17 11:11:38 +0100

    adding getSourceCode method for nestedSourceCode

--- src/utils/write-metrics.mjs
+++ src/utils/write-metrics.mjs
@@ -5,11 +5,25 @@ import ObjectsToCsv from "objects-to-csv";
 // TODO import from a conf file
 const fn_metric = "./src/csv/paso_metric.csv";
 
+const getSourceCode = data => {
+    const sourceCode = JSON.parse(data).result[0].SourceCode;
+    try {
+        const nestedSourceCode = JSON.parse(sourceCode);
+        return nestedSourceCode[
+            Object.keys(nestedSourceCode)[
+                Object.keys(nestedSourceCode).length - 1
+            ]
+        ].content;
+    } catch {
+        return sourceCode;
+    }
+};
+
 // get metrics from solidity source code
 const get_json_metrics_from_sol = dest =>
     new Promise(resolve =>
         fs.readFile(dest, (err, data) => {
-            resolve(paso(JSON.parse(data).result[0].SourceCode));
+            resolve(paso(getSourceCode(data)));
         })
     );
 
