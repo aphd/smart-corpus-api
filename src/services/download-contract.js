27505391576ce0d22b0fc5f33e05d9e6b39b8f42
apierro <antonio.pierro@gmail.com>
2020-03-18 17:15:42 +0100

    adding getSourceCode router

--- src/services/download-contract.js
+++ src/services/download-contract.js
@@ -1,35 +1,45 @@
 import https from "https";
 import fs from "fs";
 import dotenv from "dotenv";
-import { delayLoop, urlDirFn } from "../utils/index.js";
+import { delayLoop } from "../utils/index.js";
 import { getAddresses } from "./index.js";
 
 dotenv.config();
 
+const source =
+    "https://api.etherscan.io/api?module=contract&action=getsourcecode&address=";
+
+const getUrlFromAddress = contractAddress =>
+    `${source}${contractAddress}&apikey=${process.env.API_KEY}`;
+
+const dirFn = contractAddress => ({
+    dir: `./src/json/${contractAddress.substring(0, 4)}/`,
+    fn: `${contractAddress}.json`
+});
+
 export function downloadContracts() {
     getAddresses().then(addresses =>
         addresses.data.forEach(
-            delayLoop(
-                e => download(urlDirFn(e.contractAddress.toLowerCase())),
-                200
-            )
+            delayLoop(e => download(e.contractAddress.toLowerCase()), 200)
         )
     );
 }
 
 export function getSourceCode(address) {
-    console.log(address);
+    let url = dirFn(address);
+    console.log(url);
     return address;
 }
 
-const download = url_dir_fn => {
-    let { url, dir, fn } = url_dir_fn;
+const download = contractAddress => {
+    let { dir, fn } = dirFn(contractAddress);
     const dest = dir + fn;
+    const url = getUrlFromAddress(contractAddress);
 
     !fs.existsSync(dir) && fs.mkdirSync(dir);
     if (fs.existsSync(dest)) return 0;
     let file = fs.createWriteStream(dest);
-    url = `${url}&apikey=${process.env.API_KEY}`;
+
     https.get(url, function(response) {
         response.pipe(file);
         file.on("finish", () => console.log(url));
