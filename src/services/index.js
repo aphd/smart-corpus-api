190c6a4525b72e99dc799af418df709a8556077b
apierro <antonio.pierro@gmail.com>
2020-04-08 16:11:35 +0200

    adding data/metrics.json

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,9 @@
 import { downloadContracts, getContractData } from "./download-contract.js";
-import { writeMetrics } from "./handle-metrics.js";
+import { writeMetrics2CSV, writeMetrics2JSON } from "./handle-metrics.js";
 
-export { downloadContracts, getContractData, writeMetrics };
+export {
+    downloadContracts,
+    getContractData,
+    writeMetrics2CSV,
+    writeMetrics2JSON,
+};
