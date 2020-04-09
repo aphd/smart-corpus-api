33ec576c9f3bf77a49149169522b8b60af05b1cc
apierro <antonio.pierro@gmail.com>
2020-04-09 10:58:45 +0200

    updating +writeMetrics2CSV

--- src/services/client.js
+++ src/services/client.js
@@ -1,15 +1,11 @@
-import { writeMetrics2CSV, writeMetrics2JSON } from "./handle-metrics.js";
+import { writeMetrics2CSV } from "./handle-metrics.js";
 import { downloadContracts } from "./handle-contracts.js";
+import shell from "shelljs";
+const fn_metric = "./data/metrics.csv";
 
 downloadContracts();
-// writeMetrics2CSV();
-// writeMetrics2JSON();
-/* To get the distinct/unique values in metric files run this bash script
-
-    FN="./data/metrics.csv"; \
-    FNU="$FN.u"; \
-    (head -n 1 $FN && tail -n +2 $FN  | sort)  | uniq >  $FNU; \
-    mv $FNU $FN
-
-*/
-// missing writeMetricsFromCsvToJson
+writeMetrics2CSV()
+    .then((res) => console.log(res))
+    // TODO shell.exec is executed before writeMetrics2CSV
+    .then(() => shell.exec(`./src/services/remove-duplicates.sh ${fn_metric}`))
+    .catch((e) => console.error(e));
