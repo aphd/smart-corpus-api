10a6c8a3f7374bec52b7c90421103004e1540fa9
apierro <antonio.pierro@gmail.com>
2020-04-10 16:04:50 +0200

    updating compress and extract script

--- src/scripts/compress.sh
+++ src/scripts/compress.sh
@@ -1,5 +1,10 @@
 echo "compressing contracts..."
 cd data
-mv contracts.7z contracts.old.7z
+mv contracts.7z /tmp/
 7z a -t7z contracts.7z contracts
 rm -rf contracts
+
+echo "compressing metrics in CSV format..."
+mv metrics.csv.7z /tmp/ 
+7z a -t7z metrics.csv.7z metrics.csv
+rm metrics.csv
