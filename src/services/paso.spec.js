00226c36034589f8d4fd5da9708e74cae6111c0b
apierro <antonio.pierro@gmail.com>
2020-04-14 16:08:47 +0200

    adding paso spec

--- src/services/paso.spec.js
+++ src/services/paso.spec.js
@@ -1,6 +1,9 @@
-import paso from "./paso";
-describe("Getting metrics from PASO", () => {
-    it("Should ...", () => {
-        //TODO
+const get_version = require("./paso").__get__("get_version");
+describe("Metrics from PASO", () => {
+    it("Should get_version not be defined", () => {
+        expect(get_version(`"nam.2.3"`)).toEqual("Not defined");
+    });
+    it("Should get_version be defined", () => {
+        expect(get_version(`"name":"solidity","value":"^0.2.3"`)).toBeDefined();
     });
 });
