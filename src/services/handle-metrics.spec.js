211ae9cbdb54ec2bc94dff0fdd78317ba53158d5
apierro <antonio.pierro@gmail.com>
2020-04-14 17:43:18 +0200

    updating metrics

--- src/services/handle-metrics.spec.js
+++ src/services/handle-metrics.spec.js
@@ -4,9 +4,9 @@ const writeMetrics2JSON = writeMetrics.__get__("writeMetrics2JSON");
 const fsPromises = require("fs").promises;
 
 describe("Handle Metrics", () => {
-    it("Should ....", async () => {
-        let response = await fsPromises.readFile("./data/metrics.json");
-        console.log(JSON.parse(response).length);
-        expect(true).toEqual(true);
+    it("Should the JSON metric file not be empty", async () => {
+        const response = await fsPromises.readFile("./data/metrics.json");
+        const result = JSON.parse(response).length;
+        expect(result >= 0).toBeTruthy();
     });
 });
