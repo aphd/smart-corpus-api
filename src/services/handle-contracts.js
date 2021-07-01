687aa4e92b90b45bae1da89557195bba12b7d012
Antonio Pierro <antonio.pierro@gmail.com>
2021-07-01 15:43:24 +0200

    fix: fix throttling index

--- src/services/handle-contracts.js
+++ src/services/handle-contracts.js
@@ -5,17 +5,15 @@ import * as c from "../contract/contract.js";
 const downloadContracts = () => {
     try {
         const addresses = c.getContracts();
-        addresses.forEach(downloadSmartContract);
+        const addressesNonExisting = addresses.filter((e) => !doesFileExist(e));
+        addressesNonExisting.forEach(downloadSmartContract);
     } catch (e) {
         console.error("--- Error in downloading the contract ---", e);
     }
 };
 
-const downloadSmartContract = (address, i) => {
-    const doesExist = doesFileExist(address);
-    console.log({ address, i, doesExist });
-    doesExist || setTimeout(() => download(address), i * 500);
-};
+const downloadSmartContract = (address, i) =>
+    setTimeout(() => download(address), i * 500);
 
 const doesFileExist = (address) => {
     const dest = c.getDestFromAddr(address);
