07bfd84c864f0fe67bb16d7f496bd5ef51b26016
Antonio Pierro <antonio.pierro@gmail.com>
2021-07-01 15:18:13 +0200

    fix: write file only when response is good

--- src/services/handle-contracts.js
+++ src/services/handle-contracts.js
@@ -14,7 +14,7 @@ const downloadContracts = () => {
 const downloadSmartContract = (address, i) => {
     const doesExist = doesFileExist(address);
     console.log({ address, i, doesExist });
-    doesExist || setTimeout(() => download(address), i * 200);
+    doesExist || setTimeout(() => download(address), i * 500);
 };
 
 const doesFileExist = (address) => {
@@ -28,9 +28,11 @@ const download = (address) => {
     const dest = c.getDestFromAddr(address);
 
     !fs.existsSync(dir) && fs.mkdirSync(dir);
-    let file = fs.createWriteStream(dest);
 
     https.get(url, function(response) {
+        const isGoodResponse = response.headers["content-length"] > 150;
+        if (!isGoodResponse) return null;
+        let file = fs.createWriteStream(dest);
         response.pipe(file);
         file.on("finish", () => console.log(url));
     });
