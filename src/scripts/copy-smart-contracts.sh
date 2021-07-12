b94465ab6191b66a5c4d8054f84b7082bffaee58
Antonio Pierro <antonio.pierro@gmail.com>
2021-07-12 07:28:35 +0200

    fix: update copy-contracts bash script to run on ubuntu

--- src/scripts/copy-smart-contracts.sh
+++ src/scripts/copy-smart-contracts.sh
@@ -1,6 +1,7 @@
-for path in "/tmp/smartcontracts/"*
+#!/bin/bash
+for path in /tmp/smartcontracts/*.sol
 do
     file_name="${path##*/}"
-    file_name_lower=$(echo "${file_name}" | awk '{print tolower($0)}')
+    file_name_lower=`echo "${file_name}" | awk '{print tolower($0)}'`
     cp ${path} data/contracts/${file_name_lower:0:4}/${file_name_lower}
 done
