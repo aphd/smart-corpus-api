85df30fe8bcbf06031425777426ad0125df1a0a0
Antonio Pierro <antonio.pierro@gmail.com>
2022-04-15 16:51:23 +0200

    feat: add R debugger and plot_pow

--- data-model/main.R
+++ data-model/main.R
@@ -1,12 +1,17 @@
-df = read.csv('../data/metrics.csv', stringsAsFactors=T)
+DF = read.csv('./data/metrics.csv', stringsAsFactors=T)
+BIN_NUMS = 100
 
-plot_hist <- function(column_name) {
-    tmp_df <- df[!df[[column_name]] == "n/a", ]
-    x <- as.numeric(tmp_df[[column_name]])
+get_data <- function(column_name) {
+    tmp_df <- DF[!DF[[column_name]] == "n/a", ]
+    return(as.numeric(tmp_df[[column_name]]))
+}
+
+plot_hist_by_column_name <- function(column_name) {
+    x = get_data(column_name)
     hist(x, breaks = 100, main = paste("Hist of", column_name), xlab = NULL )
 }
 
-main <- function() {
+plot_hist <- function() {
     nRow <- 4
     nCol <- 5
     notNames = "rawVersion|assemblyStatement|doWhileStatement|block|public|version|revertStatement|simpleStatement|tryStatement|whileStatement|isFallback|throwStatement|isVirtual|pure|payable|libraries|interfaces"
@@ -14,5 +19,41 @@ main <- function() {
     # layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE))
     names <- colnames(df)
     names <- names[!grepl(notNames, names)]
-    sapply(names[0:(nRow * nCol)], plot_hist)
+    sapply(names[0:(nRow * nCol)], plot_hist_by_column_name)
 }
+
+plot_pow_by_column_name <- function(column_name) {
+    cbindData = get_cumulative_freq(column_name)
+    y <- cbindData[,"Cumul"]
+    x <- seq(1, length(y), 1)
+    plot(log(y), x, main=paste("Plot of", column_name))
+}
+
+get_freq <- function(column_name) {
+    data = get_data(column_name)
+    maxData = max(data) 
+    minData = min(data) 
+    step = (maxData - minData) / BIN_NUMS 
+    freq = data.frame(table(cut(data, breaks=seq(minData - 1, maxData + 1 , by = step  ))))
+    return(freq)
+}
+
+get_cumulative_freq <- function(column_name) {
+    x <- get_data(column_name)
+    cbindData <- cbind( Freq=table(x), Cumul=cumsum(table(x)), relative=prop.table(table(x)))
+    return(cbindData)
+}
+
+plot_pow <- function() {
+    # column_name = "ifStatement"
+    # plot_pow_by_column_name(column_name)
+    nRow <- 4
+    nCol <- 5
+    notNames = "rawVersion|assemblyStatement|doWhileStatement|block|public|version|revertStatement|simpleStatement|tryStatement|whileStatement|isFallback|throwStatement|isVirtual|pure|payable|libraries|interfaces"
+    par(mfrow = c(nRow, nCol))
+    names <- colnames(DF)
+    names <- names[!grepl(notNames, names)]
+    sapply(names[0:(nRow * nCol)], plot_pow_by_column_name)
+}
+
+plot_pow()
